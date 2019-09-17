05ccde8ecd07e35d2d851b8e6dfd5915ed0544d5
Andy <github@sockbot.com>
2019-09-17 10:38:26 -0700

    complete final exercise

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter a store name: "
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+puts new_store.errors.messages[:annual_revenue]
+
+# Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
