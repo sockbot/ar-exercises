1a0bbf24933e15104d134301a74650f5795f4468
Andy <github@sockbot.com>
2019-09-17 10:06:13 -0700

    complete 6th exercise

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Travis", last_name: "Borsa", hourly_rate: 60)
+@store1.employees.create(first_name: "Nima", last_name: "Boscarino", hourly_rate: 60)
+@store2.employees.create(first_name: "Aaron", last_name: "Dufall", hourly_rate: 60)
+@store2.employees.create(first_name: "Kelly", last_name: "Gordon", hourly_rate: 60)
+@store2.employees.create(first_name: "Karl", last_name: "Jensen", hourly_rate: 60)
